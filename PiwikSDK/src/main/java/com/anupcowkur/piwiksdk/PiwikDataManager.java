735d1d0e890cfe4631547a7260171bfe2e8b4f22
Karan Kishore <karan.ks@gmail.com>
2013-09-14 23:29:51 +0530

    Finished DB schema and AsyncTask to store data in piwik api format

--- PiwikSDK/src/main/java/com/anupcowkur/piwiksdk/PiwikDataManager.java
+++ PiwikSDK/src/main/java/com/anupcowkur/piwiksdk/PiwikDataManager.java
@@ -4,36 +4,39 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class PiwikDataManager extends SQLiteOpenHelper{
+public class PiwikDataManager extends SQLiteOpenHelper {
 
     private static PiwikDataManager piwikDataManager = null;
 
     private static final String DATABASE_NAME = "piwik.db";
     private static final int DATABASE_VERSION = 1;
-    public static final String TABLE_NAME = "event";
-    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, info TEXT";
-    private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    public static final String EVT_TABLE = "event";
+    public static final String EVT_TABLE_COL_INFO = "info";
+    public static final String EVT_TABLE_COL_EXTRA_INFO = "extra_info";
+    public static final String EVT_TABLE_COL_TIMESTAMP = "timestamp";
+    private static final String CREATE_TABLE = "CREATE TABLE " + EVT_TABLE + "_id INTEGER PRIMARY KEY AUTOINCREMENT," + EVT_TABLE_COL_INFO + " TEXT, " + EVT_TABLE_COL_EXTRA_INFO + " TEXT, " + EVT_TABLE_COL_TIMESTAMP + " TEXT";
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS " + EVT_TABLE;
 
-    public static PiwikDataManager getInstance(Context context){
-        if(piwikDataManager == null){
+    public static PiwikDataManager getInstance(Context context) {
+        if (piwikDataManager == null) {
             piwikDataManager = new PiwikDataManager(context);
         }
         return piwikDataManager;
     }
 
 
-        public PiwikDataManager(Context context){
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
+    public PiwikDataManager(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-        @Override
-        public void onCreate(SQLiteDatabase sqLiteDatabase) {
-            sqLiteDatabase.execSQL(CREATE_TABLE);
-        }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+    }
 
-        @Override
-        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
-            sqLiteDatabase.execSQL(DROP_TABLE);
-            onCreate(sqLiteDatabase);
-        }
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+        sqLiteDatabase.execSQL(DROP_TABLE);
+        onCreate(sqLiteDatabase);
+    }
 }
