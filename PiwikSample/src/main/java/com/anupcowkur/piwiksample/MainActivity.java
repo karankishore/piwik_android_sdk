6de16f46ced626a1a7900ccb5d4621ac4798fd6d
Anup Cowkur <anupcowkur89@gmail.com>
2013-09-15 07:46:32 +0530

    added a modified android animations demo as sample app

--- PiwikSample/src/main/java/com/anupcowkur/piwiksample/MainActivity.java
+++ PiwikSample/src/main/java/com/anupcowkur/piwiksample/MainActivity.java
@@ -1,46 +1,49 @@
 package com.anupcowkur.piwiksample;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.anupcowkur.piwiksdk.PiwikClient;
+public class MainActivity extends ListActivity {
 
-public class MainActivity extends Activity {
+    private static Sample[] mSamples;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PiwikClient.initPiwik(this, "http://www.mantish.com/piwik/piwik.php", null);
-        PiwikClient.trackEvent(this, "home", null);
-        findViewById(R.id.btn_activityMain).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ActivityB.class));
-                finish();
-            }
-        });
+
+        // Instantiate the list of samples.
+        mSamples = new Sample[]{new Sample(R.string.title_crossfade, CrossfadeActivity.class), new Sample(R.string.title_card_flip, CardFlipActivity.class), new Sample(R.string.title_screen_slide, ScreenSlideActivity.class), new Sample(R.string.title_zoom, ZoomActivity.class), new Sample(R.string.title_layout_changes, LayoutChangesActivity.class),};
+
+        setListAdapter(new ArrayAdapter<Sample>(this, android.R.layout.simple_list_item_1, android.R.id.text1, mSamples));
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onListItemClick(ListView listView, View view, int position, long id) {
+        // Launch the sample associated with this list position.
+        startActivity(new Intent(MainActivity.this, mSamples[position].activityClass));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    /**
+     * This class describes an individual sample (the sample title, and the activity class that
+     * demonstrates this sample).
+     */
+    private class Sample {
+        private CharSequence title;
+        private Class<? extends Activity> activityClass;
 
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                PiwikClient.syncImmediately();
-                break;
+        public Sample(int titleResId, Class<? extends Activity> activityClass) {
+            this.activityClass = activityClass;
+            this.title = getResources().getString(titleResId);
         }
-        return super.onOptionsItemSelected(item);
+
+        @Override
+        public String toString() {
+            return title.toString();
+        }
     }
 }
